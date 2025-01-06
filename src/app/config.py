76ac102e28673517582150f892622bc6f5ff1154
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-06 21:03:37 +0300

    Rename class

--- src/app/config.py
+++ src/app/config.py
@@ -5,13 +5,13 @@ from dataclasses import dataclass
 
 
 @dataclass
-class BotSecret:
+class BotConfig:
     token: str
     id_admin: int
 
 
-def load_bot_secret() -> BotSecret:
-    return BotSecret(
+def load_bot_secret() -> BotConfig:
+    return BotConfig(
         token=os.getenv('TOKEN'),
         id_admin=int(os.getenv('ID_ADMIN')),
     )
