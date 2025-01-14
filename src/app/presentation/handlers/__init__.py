f6cf1afe28e8a72c7792083ce27424e80c705b1e
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-14 16:35:16 +0300

    Add imports

---
+++ src/app/presentation/handlers/__init__.py
@@ -0,0 +1,40 @@
+from .adding_question_handler import AddingQuestionHandler
+from .average_score_handler import AverageScoreHandler
+from .birthdate_handler import BirthdateHandler
+from .document_handler import DocumentHandler
+from .faq_handler import FaqHandler
+from .field_study_handler import FieldStudyHandler
+from .id_handler import IDHandler
+from .incoming_question_handler import IncomingQuestionHandler
+from .military_station_handler import MilitaryStationHandler
+from .name_handler import NameHandler
+from .patronymic_handler import PatronymicHandler
+from .find_out_handler import FindOutHandler
+from .question_handler import QuestionHandler
+from .recruitment_handler import TypeRecruitmentHandler
+from .sending_document_handler import SendingDocumentHandler
+from .start_handler import StartHandler
+from .surname_handler import SurnameHandler
+from .telegram_handler import TelegramChannelHandler
+from .university_handler import UniversityHandler
+
+__all__ = (
+    'AddingQuestionHandler',
+    'AverageScoreHandler',
+    'BirthdateHandler',
+    'DocumentHandler',
+    'FaqHandler',
+    'FieldStudyHandler',
+    'IDHandler',
+    'IncomingQuestionHandler',
+    'MilitaryStationHandler',
+    'PatronymicHandler',
+    'FindOutHandler',
+    'QuestionHandler',
+    'TypeRecruitmentHandler',
+    'SendingDocumentHandler',
+    'StartHandler',
+    'SurnameHandler',
+    'TelegramChannelHandler',
+    'UniversityHandler'
+)
