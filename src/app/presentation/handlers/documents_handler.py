efff0f3bb1a6ce45bbd69fd2e26a08cfca6679a0
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-14 13:18:49 +0300

    Update document handler

--- src/app/presentation/handlers/documents_handler.py
+++ src/app/presentation/handlers/documents_handler.py
@@ -1,17 +1,17 @@
 from telebot import TeleBot
 from telebot.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
-from app.constants import FEDERAL_ACT, CONSULTANT_URL
-
 
 def documents_handler(message: Message, bot: TeleBot) -> None:
     markup = InlineKeyboardMarkup()
     button1 = InlineKeyboardButton(
         'Ссылка',
-        url=CONSULTANT_URL,
+        url='https://www.consultant.ru/document/cons_doc_LAW_18260/',
     )
     markup.add(button1)
 
-    bot.send_message(message.chat.id,
-                     text=FEDERAL_ACT,
-                     reply_markup=markup)
+    bot.send_message(
+        message.chat.id,
+        text='ФЗ № 53 «О воинской обязанности и военной службе» от 28.03.1998 (ред. 02.10.2024)',
+        reply_markup=markup
+    )
