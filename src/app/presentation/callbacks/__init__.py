8b78c71a9389a784387261ce4f775c2d38ac65e1
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-14 13:17:27 +0300

    Create packets

---
+++ src/app/presentation/callbacks/__init__.py
@@ -0,0 +1,5 @@
+from .nationality_callback import NationalityCallback
+from .recruitment_callback import RecruitmentCallback
+from .university_callback import UniversityCallback
+
+__all__ = ('UniversityCallback', 'RecruitmentCallback', 'NationalityCallback')
