a6ccf705e6d55ae836a5b82381bfb18fc4054d28
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-16 15:16:49 +0300

    Update imports

--- src/app/presentation/callbacks/__init__.py
+++ src/app/presentation/callbacks/__init__.py
@@ -1,5 +1,15 @@
+from .answer_callback import AnswerCallback
 from .nationality_callback import NationalityCallback
+from .pagination_callback import PaginateCallback
+from .question_callback import QuestionCallback
 from .recruitment_callback import RecruitmentCallback
 from .university_callback import UniversityCallback
+from .unseen_callback import UnseenCallback
 
-__all__ = ('UniversityCallback', 'RecruitmentCallback', 'NationalityCallback')
+__all__ = ('UniversityCallback',
+           'RecruitmentCallback',
+           'NationalityCallback',
+           'PaginateCallback',
+           'UnseenCallback',
+           'AnswerCallback',
+           'QuestionCallback')
