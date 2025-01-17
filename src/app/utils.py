7aaeaeb11e65a58b97d15eeece9bf13bd4df900d
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-17 17:22:04 +0300

    Fix bug

--- src/app/utils.py
+++ src/app/utils.py
@@ -7,4 +7,5 @@ from openpyxl import load_workbook
 def excel_to_2d_array(path: Path) -> pd.DataFrame:
     wb = load_workbook(path)
     sheet = wb.active
+    wb.close()
     return pd.DataFrame(sheet.values)
