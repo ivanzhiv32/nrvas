585add9ca40a3b2274bf56eb476d3cf968dc9dee
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-19 15:02:52 +0300

    Update handler

--- src/app/presentation/handlers/adding_question_handler.py
+++ src/app/presentation/handlers/adding_question_handler.py
@@ -9,13 +9,21 @@ class AddingQuestionHandler(IHandler):
     def __call__(self, message: Message, bot: TeleBot) -> None:
         bot.send_message(
             message.from_user.id,
-            'Спасибо за вопрос, он добавлен в базу. В скором времени на него ответят и вам придет уведомление.'
+            'Спасибо за вопрос, он добавлен в базу. '
+            'В скором времени на него ответят и вам придет уведомление.'
         )
+        self._add_question(message.from_user.id, message.text)
+        bot.send_message(
+            self.ioc.id_admin,
+            'Пользователь задал вопрос'
+        )
+
+    def _add_question(self, user_id: int, question: str) -> None:
         # TODO: добавить БД
         filename = 'documents/questions.xlsx'
         wb = load_workbook(self.ioc.path / filename)
         sheet = wb.active
-        rows_count = sheet.max_row
-        data = (rows_count, message.from_user.id, False, message.text)
+        count = sheet.max_row
+        data = (count, user_id, False, question)
         sheet.append(data)
         wb.save(self.ioc.path / filename)
