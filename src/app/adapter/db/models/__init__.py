60f060efa581b39d3434656823ae2baa6c8558a2
Alexander (Wolf) <qrer71@yandex.ru>
2025-01-20 20:46:37 +0300

    Update imports

--- src/app/adapter/db/models/__init__.py
+++ src/app/adapter/db/models/__init__.py
@@ -1,2 +1,7 @@
 from .base import BaseModel
+from .candidate import CandidateStorage
 from .faq import FAQStorage
+
+__all__ = ('BaseModel',
+           'FAQStorage',
+           'CandidateStorage')
